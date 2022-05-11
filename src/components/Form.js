f0f686602b850e0dcd0e89b0e9009683ea05762c
Romanov7788 <romanov_sash@ukr.net>
2022-05-11 12:17:27 +0300

    Finish
    Переписал

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,88 +1,74 @@
-import Typography from '@mui/material/Typography';
-import { makeStyles } from '@mui/styles';
-import React, { useState } from 'react';
-import Weather from './Weather';
+import Typography from "@mui/material/Typography";
+import { makeStyles } from "@mui/styles";
+import React, { useState } from "react";
+import Weather from "./Weather";
+
+const useStyles = makeStyles((theme) => ({
+  main: {
+    backgroundColor: "yellow",
+  },
+  title: {
+    fontSize: "50px",
+    display: "flex",
+    justifyContent: "center",
+  },
+}));
+
+function Form() {
+
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const API_URL1 = process.env.REACT_APP_API_URL1;
+
+  const classes = useStyles();
+
+  const [city, setCity] = useState();
+
+  const [weather, setWeather] = useState();
 
 
-const useStyles = makeStyles(theme => ({
-    main: {
-      backgroundColor: 'yellow'
-    },
-    title: {
-      fontSize: '62px',
-      display: 'flex',
-      justifyContent: 'center'
+  async function weatherData(e) {
+    e.preventDefault();
+    if (city === "") {
+      alert("ВВЕДИТЕ ГОРОД");
+    } else {
+      fetch(`${API_URL1}weather?q=${city}&appid=${API_KEY}&units=metric`)
+        .then((res) => res.json())
+        .then((data) => setWeather(data));
     }
-  }));
-  
-
-
-  function Form () {
-
-    // спятал ключ
-    const API_KEY = process.env.REACT_APP_API_KEY
-  
-    const classes = useStyles(); 
-  
-    const [form, setForm] = useState({
-      city:""
-    });
-  
-    const [weather, setWeather] = useState([])
-  
-    async function weatherData(e){
-      e.preventDefault();
-      if (form.city === "") {
-        alert("ВВЕДИТЕ ГОРОД");
-      } else {
-        const data = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?q=${form.city}&appid=${API_KEY}&units=metric`
-          )
-          .then((res) => res.json())
-          .then((data) => data);
-  
-          setWeather(
-            {
-              data : data
-            }
-          );
-      }
-    }
-  
-    const handleChange = (e) => {
-      let name = e.target.name;
-      let value = e.target.value;
-  
-      if (name === "city"){
-        setForm({...form, city:value})
-      }
-    };
-  
-    return (
-  
-        <div className={classes.main}>
-          <Typography className={classes.title}>Погода</Typography>
-      
-  
-        <div>
-          <form>
-            <center>
-          <input type="text" name="city" placeholder="Например Харьков" onChange={e => handleChange(e)}/>
-            </center>
-            <center>
-          <button className="getweather" onClick={e => weatherData(e)}>Поиск</button>
-            </center>
-          </form>
-              <br />
-        </div>
-            {weather.data !== undefined ? (
-        <div>
-            <Weather data={weather.data} />
-          </div>
-        ) : null}
-      </div>
-    );
   }
-  
-  
-  export default Form;
+
+  const handleChange = (e) => setCity(e.target.value);
+
+  return (
+    <div className={classes.main}>
+      <Typography className={classes.title}>Погода</Typography>
+
+      <div>
+        <form>
+          <center>
+            <input
+              className="title"
+              type="text"
+              name="city"
+              placeholder="Например Харьков"
+              onChange={(e) => handleChange(e)}
+            />
+          </center>
+          <center>
+            <button onClick={(e) => weatherData(e)}>
+              Поиск
+            </button>
+          </center>
+        </form>
+        <br />
+      </div>
+      {weather != undefined ? (
+        <div>
+          <Weather weather={weather} />
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default Form;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,111 +1,72 @@
 import React from "react";
+
 import "./displayweather.css";
 
-function Weather(props) {
-    const {data} = props;
-    console.log(data);
 
-    const iconurl = "http://openweathermap.org/img/wn/" + `${data.weather[0].icon}` + ".png"
-    return <div className="displayweather">
-            <div className="maincard">
-                <span className="cardtitle">
-                    Город: {data.name} 
-                </span>
-                <span className="cardsubtitle">
-              Обновленно: {new Date().toLocaleTimeString()}
-            </span>
-                    <h1>
-                        {Math.floor(data.main.temp)}°
-                    </h1>
-                    {/* <span classNAme="weather-main"> {data.weather[0].main}</span> */}
-                    <img src={iconurl} className="weather-icon" alt=""/>
-    
-     <div className="weatherdetails">
-            <div className="section1">
-              <table>
-              <tr>
-                  <td>
-                    <h4>Ощущаться как</h4>
-                  </td>
-                  <td>
-                    <span>{Math.floor(data.main.feels_like)}°</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h4>Влажность</h4>
-                  </td>
-                  <td>
-                    <span>{data.main.humidity} %</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h4>Давление</h4>
-                  </td>
-                  <td>
-                    <span>{data.main.pressure} hPa</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h4>Видимость</h4>
-                  </td>
-                  <td>
-                    <span>{data.visibility / 1000} Km</span>
-                  </td>
-                </tr>
-              </table>
-            </div>
+const Weather = ({ weather }) => {
+  const API_URL = process.env.REACT_APP_API_URL;
 
-            <div className="section2">
-              <table>
-                <tr>
-                  <td>
-                    <h4>Скорость</h4>
-                  </td>
-                  <td>
-                    <span>{Math.floor((data.wind.speed * 18) / 5)} km/hr</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h4>Направление ветра</h4>
-                  </td>
-                  <td>
-                    <span>
-                      {data.wind.deg}
-                      <sup>o</sup> deg
-                    </span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h4>Восход солнца</h4>
-                  </td>
-                  <td>
-                    <span>
-                      {new Date(data.sys.sunrise * 1000).toLocaleTimeString()}
-                    </span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h4>Заход солнца</h4>
-                  </td>
-                  <td>
-                    <span>
-                      {new Date(data.sys.sunset * 1000).toLocaleTimeString()}
-                    </span>
-                  </td>
-                </tr>
-              </table>
-            </div>
-          </div>
-          </div>
-          <br /><br />
-          </div>
+  const iconurl = `${API_URL}` + `${weather.weather[0].icon}` + ".png";
 
-}
+  const renderDataBloc = (title, date) => {
+    return (
+      <tr>
+        <td>
+          <h4>{title}</h4>
+        </td>
+        <td>
+          <span>{date}</span>
+        </td>
+      </tr>
+    );
+  };
+
+  return (
+    <div className="displayweather">
+      <div className="maincard">
+        <span className="cardtitle">Город: {weather.name}</span>
+        <span className="cardsubtitle">
+          Обновленно: {new Date().toLocaleTimeString()}
+        </span>
+        <h1>{Math.floor(weather.main.temp)}°</h1>
+        {/* <span classNAme="weather-main"> {weather.weather[0].main}</span> */}
+        <img src={iconurl} className="weather-icon" alt="" />
+
+        <div className="weatherdetails">
+          <div className="section1">
+            <table>
+              {renderDataBloc(
+                "Ощущаться как",
+                Math.floor(weather.main.feels_like)
+              )}
+              {renderDataBloc("Влажность %", weather.main.humidity)}
+              {renderDataBloc("Давление hPa", weather.main.pressure)}
+              {renderDataBloc("Видимость Км", weather.visibility / 1000)}
+            </table>
+          </div>
+          <div className="section2">
+            <table>
+              {renderDataBloc(
+                "Скорость ветра Км/час",
+                Math.floor((weather.wind.speed * 18) / 5)
+              )}
+              {renderDataBloc("Направление ветра", weather.wind.deg)}
+              {renderDataBloc(
+                "Восход солнца",
+                new Date(weather.sys.sunrise * 1000).toLocaleTimeString()
+              )}
+              {renderDataBloc(
+                "Заход солнца",
+                new Date(weather.sys.sunset * 1000).toLocaleTimeString()
+              )}
+            </table>
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
+    </div>
+  );
+};
 
 export default Weather;
